0e58a2df6c244089f7343b2bc9614dc48005593d
Rohit Rajhans <rohitrajhans07@gmail.com>
2018-01-12 15:03:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function display12() {
+	document.getElementsByClassName("headerName")[0].style.display = "none";
+	document.getElementsByClassName("headerHobbies")[0].style.display = "block";
+} 
+function display13() {
+	document.getElementsByClassName("headerName")[0].style.display = "none";
+	document.getElementsByClassName("headerAchievements")[0].style.display = "block";
+} 
+function display21() {
+	document.getElementsByClassName("headerHobbies")[0].style.display = "none";
+	document.getElementsByClassName("headerName")[0].style.display = "block";
+} 
+function display23() {
+	document.getElementsByClassName("headerHobbies")[0].style.display = "none";
+	document.getElementsByClassName("headerAchievements")[0].style.display = "block";
+} 
+function display31() {
+	document.getElementsByClassName("headerAchievements")[0].style.display = "none";
+	document.getElementsByClassName("headerName")[0].style.display = "block";
+} 
+function display32() {
+	document.getElementsByClassName("headerAchievements")[0].style.display = "none";
+	document.getElementsByClassName("headerHobbies")[0].style.display = "block";
+} 
